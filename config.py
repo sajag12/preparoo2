8d3c935311f490034b45397e21b163da9bb6fd6c
sajag12 <sajag12prakash@gmail.com>
2025-06-11 03:10:02 +0530

    Add DEVELOPMENT environment variable for local OAuth development

--- config.py
+++ config.py
@@ -16,6 +16,8 @@ class Config:
     GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
     GOOGLE_OAUTH_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET')
     
-    # OAuth settings - Force HTTPS everywhere except localhost
-    OAUTHLIB_INSECURE_TRANSPORT = False  # Always use HTTPS
+    # OAuth settings
+    # Set DEVELOPMENT=true in your .env file for local development
+    # This allows HTTP for localhost, otherwise HTTPS is enforced
+    OAUTHLIB_INSECURE_TRANSPORT = os.environ.get('DEVELOPMENT', '').lower() == 'true'
     OAUTHLIB_RELAX_TOKEN_SCOPE = True 
